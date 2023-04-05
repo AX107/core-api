fec0ade6764c6c3f86deda125ba3f8d5ca9a0538
himanshusharma <VE00AX107@globalymc.com>
2023-04-05 11:16:51 +0530

    list is updates

--- dev-ops-core-api/Controllers/WeatherForecastController.cs
+++ dev-ops-core-api/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Test"
+        "Freezing", "Bracing", "Chilly", "Cool", "Test"
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
